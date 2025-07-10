f3cec77d1d5f0ee0b54feef44b149a9515efa8fd
Kiều Thanh Nghị <souleater2222001@gmail.com>
2025-07-10 21:18:31 +0700

    demo8 ver2

--- Assets/_Asset/Script/EnemyWave.cs
+++ Assets/_Asset/Script/EnemyWave.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Wave", menuName = "My ScriptableObject/DataEnemyWave")]
+
 [System.Serializable]
-public class EnemyWave
+public class EnemyWave: ScriptableObject
 {
     public Transform enemyPrefab;
     public int numberOfEnemy;
@@ -9,5 +11,16 @@ public class EnemyWave
     public FlyPath flyPath;
     public float speed;
     public float nextWaveDelay;
+
+    public void OnDrawGizmos()
+    {
+        if (flyPath.waypoints == null) return;
+
+        Gizmos.color = Color.green;
+        for (int i = 0; i < flyPath.waypoints.Length - 1; i++)
+        {
+            Gizmos.DrawLine(flyPath.waypoints[i].transform.position, flyPath.waypoints[i + 1].transform.position);
+        }
+    }
 }
 
--- Assets/_Asset/Script/PlayerMovement.cs
+++ Assets/_Asset/Script/PlayerMovement.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] Vector3 fingerPointOffset;
+    [SerializeField] float flightSpeed;
     // Update is called once per frame
     void Update()
     {
-        var worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition) + fingerPointOffset;
         worldPoint.z = 0;
-        transform.position = worldPoint;
+        transform.
+        transform.position = Vector3.MoveTowards(transform.position, worldPoint, flightSpeed * Time.deltaTime);
     }
 }
